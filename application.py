5315304762df4d01be5fec7058c621bfc675fdc9
DziyanaT <dziyana.tsetserava@etu.umontpellier.fr>
2023-05-24 14:01:05 +0200

    feat: connect app to database

--- application.py
+++ application.py
@@ -4,15 +4,19 @@ import json
 
 app = Flask(__name__)
 
-#Establishing the database connection
-conn = psycopg2.connect(
-   database="mydb", user='postgres', password='password', host='127.0.0.1', port= '5432'
-)
+conn = 0
+
+try: 
+    #Establishing the database connection
+    conn = psycopg2.connect(
+    database="city_api", user='test', password='test', host='127.0.0.1', port='5435'
+    )
+except Exception as err:
+    print(err)
+    raise RuntimeError("Cannot connect to the database")
 
 # Open a cursor to perform database operations
 cur = conn.cursor()
-
-
 
 @app.route("/_health")
 def health():
@@ -21,13 +25,21 @@ def health():
 @app.route("/city", methods=['POST', 'GET'])
 def city():
     if request.method == 'POST':
-        cur.execute("INSERT INTO city (c1, c2, c3) VALUES(%s, %s, %s)", (1, 2, 3))
-        conn.commit()
+        try:
+            data = request.json
+            cur.execute("INSERT INTO city (department_code, insee_code, zip_code, name, lat, lon) VALUES (%s, %s, %s, %s, %s, %s)",
+            (data.get('department_code'), data.get('insee_code'), data.get('zip_code'), data.get('name'), data.get('lat'), data.get('lon')))
+            conn.commit()
+        except Exception as err:
+            return str(err), 500
         return "", 201
     else:
-        # Execute a query
-        cur.execute("SELECT * FROM city")
+        try:
+            # Execute a query
+            cur.execute("SELECT * FROM city")
 
-        # Retrieve query results
-        cities = cur.fetchall()
+            # Retrieve query results
+            cities = cur.fetchall()
+        except Exception as err:
+            return str(err), 500
         return json.dumps(cities), 200
