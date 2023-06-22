2edf523d1c114333d9fb89d4cec377f1ceb2ec17
DziyanaT <dziyana.tsetserava@etu.umontpellier.fr>
2023-06-22 14:22:19 +0200

    fix: add comments to functions for pylint

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,3 +1,5 @@
+"""Module for test configurations."""
+from application import app as flask_app
 import os
 import sys
 import pytest
@@ -7,13 +9,14 @@ parent_directory = os.path.dirname(current)
 
 sys.path.append(parent_directory)
 
-from application import app as flask_app
 
 @pytest.fixture
 def app():
+    """Function creating an app fixture for tests."""
     yield flask_app
 
 
 @pytest.fixture
 def client(app):
+    """Function creating a client fixture for tests."""
     return app.test_client()
--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,14 +1,20 @@
+"""Module for app tests."""
+
+
 def test_healthcheck(client):
+    """Function testing the app existence."""
     response = client.get("/_health")
     assert response.status_code == 204
 
 
 def test_get_cities(client):
+    """Function testing the GET city endpoint."""
     response = client.get("/city")
     assert response.status_code == 200
 
 
 def test_insert_city(client):
+    """Function testing the city insertion endpoint."""
     response = client.post("/city", json={
         "department_code": "989",
         "name": "Île de Clipperton",
